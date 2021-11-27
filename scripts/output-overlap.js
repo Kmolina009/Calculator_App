ab54d52539c41a205eaec98e9ee2ed76ca5a0075
Keven <kevenmolina@gmail.com>
2021-11-26 22:04:44 -0500

    output-overlap script added

--- scripts/output-overlap.js
+++ scripts/output-overlap.js
@@ -1,14 +1,17 @@
 /*
-    check output div's p tag
-*/ 
+This script is meant to change the styling of a 
+float element if the number of characters in the 
+.calc-output element is more than 15 chars long.
+*/
 let calcOutputString = document.querySelector(".calc-output > p");
-function outputLengthCheck(e){
-    // console.log(`${e.target.className} has been clicked`);
-    if (e.target.className==="calc-key") {
-        console.log("CALC KEEEEEEEEEEEEEEEEEY")
-        calcOutputString.innerText.length
-}
+function outputLengthCheck(e) {
+  // console.log(`${e.target.className} has been clicked`);
+  if (e.target.className === "calc-key") {
+    console.log("CALC KEEEEEEEEEEEEEEEEEY");
+    calcOutputString.innerText.length;
+  }
 }
 
-document.querySelector(".calc-button-grid")
-.addEventListener('click', outputLengthCheck)
+document
+  .querySelector(".calc-button-grid")
+  .addEventListener("click", outputLengthCheck);
